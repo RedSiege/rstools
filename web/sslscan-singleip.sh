6101ac9e6b7d056f078665ba0775d74afefdcf82
Tim Medin <tim@redsiege.com>
2019-11-13 19:05:01 -0600

    fix sslv3 detection bug

--- web/sslscan-singleip.sh
+++ web/sslscan-singleip.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
+SSLSCANPATH=sslscan
+
+if [ $# -ne 1 ] || [[ "$*" == *-h* ]] || [[ "$*" == *--help* ]]; then
+    echo "Usage: $0 [host:port | host]"
+    exit 0
+fi
+
+if [[ $1 == *:* ]]; then
+        TARGET=$1
+else
+        TARGET=$1:443
+fi
+
+FILENAME=`echo $TARGET | sed -e 's/:/_/g'`.xml
+
+if [ -f $FILENAME ]; then
+        echo -e '\033[0;31m'$FILENAME already exists, skipping sslscan'\033[0m'
+else
+        $SSLSCANPATH --xml=$FILENAME $1
+        echo
+        echo
+fi
 
 xmlstarlet sel -T -t -m //ssltest -o 'Target: ' -v @host -o ':' -v @port -n \
         -i "///certificate/self-signed[text()='true']" -o 'Self-signbed Certificate' -n -b \
         -i "///certificate/expired[text()='true']" -o 'Expired Certificate' -n -b \
         -i "///certificate/signature-algorithm[contains(text(), 'sha1')]" -o 'Bad Certificate Signature Algorithm: SHA1' -n -b \
         -m "///certificate/pk[@bits<2048]" -o 'Weak RSA ' -v @bits -o ' bits' -n \
-        $1
+        $FILENAME
 xmlstarlet sel -T -t \
-        -i "//ssltest/cipher[contains(sslversion, 'SSL') or @sslversion='TLSv1.0' or @sslversion='TLSv1.1']" -o 'Deprecated Protocols:' -n -b \
-        -m "//ssltest/cipher[contains(sslversion, 'SSL') or @sslversion='TLSv1.0' or @sslversion='TLSv1.1']" -v '@sslversion' -n -b \
-        $1 \
+        -i "//ssltest/cipher[contains(@sslversion, 'SSL') or @sslversion='TLSv1.0' or @sslversion='TLSv1.1']" -o 'Deprecated Protocols:' -n -b \
+        -m "//ssltest/cipher[contains(@sslversion, 'SSL') or @sslversion='TLSv1.0' or @sslversion='TLSv1.1']" -v '@sslversion' -n -b \
+        $FILENAME \
         | sort -u
 xmlstarlet sel -T -t \
         -i "//ssltest/cipher[contains(@cipher,'NULL') or contains(@cipher,'EXP') or contains(@cipher,'ADH') or contains(@cipher,'AECDH') and (contains(@sslversion,'TLSv1.0') or contains(@sslversion,'TLSv1.1') or contains(@sslversion,'TLSv1.2'))]" -o 'Red Ciphers' -n -b \
-        $1
+        $FILENAME
 xmlstarlet sel -T -t \
         -m "//ssltest/cipher[contains(@cipher,'NULL') or contains(@cipher,'EXP') or contains(@cipher,'ADH') or contains(@cipher,'AECDH') and (contains(@sslversion,'TLSv1.0') or contains(@sslversion,'TLSv1.1') or contains(@sslversion,'TLSv1.2'))]" -v @sslversion -o ":" -v @cipher -n -b \
-        $1 \
+        $FILENAME \
         | cut -d: -f 2
 
 xmlstarlet sel -T -t \
         -i "//ssltest/cipher[contains(@cipher,'RC4') or contains(@cipher,'RC4')]" -o 'Yellow Ciphers' -n -b \
-        $1
+        $FILENAME
 xmlstarlet sel -T -t \
         -m "//ssltest/cipher[(contains(@cipher,'RC4') or contains(@cipher,'RC4')) and (contains(@sslversion,'TLSv1.0') or contains(@sslversion,'TLSv1.1') or contains(@sslversion,'TLSv1.2'))]" -v @sslversion -o ":" -v @cipher -n -b \
-        $1 \
+        $FILENAME \
         | cut -d: -f 2 | sort -u
